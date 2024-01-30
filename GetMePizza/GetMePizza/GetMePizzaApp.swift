00a59d1bfc6f4f5c2a9e2adcc0c33d36c056fd46
Akhil <akhil855singh@gmail.com>
2024-01-30 21:43:41 +0530

    added project.

---
+++ GetMePizza/GetMePizza/GetMePizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMePizzaApp.swift
+//  GetMePizza
+//
+//  Created by Akhil on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GetMePizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
